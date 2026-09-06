48e264db76886b1354810279215201f198200631
Duckycash <jaegerhi78@gmail.com>
2026-09-06 21:38:16 +0700

    Delete src directory

--- src/config.lua
+++
@@ -1,8 +0,0 @@
-local Config = {
-    Version = "1.0.0",
-    TimeoutPerTest = 3, -- seconds before a stuck test times out
-    DebugConsole = true,
-    GithubRawUrl = "https://raw.githubusercontent.com/YourUsername/xUNC/main/build/init.lua"
-}
-
-return Config
