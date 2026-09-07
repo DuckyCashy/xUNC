fabe675eaedfdec81f9fcf6689eb88ac660cb590
Duckycash <jaegerhi78@gmail.com>
2026-09-07 09:48:55 +0700

    Add configuration file with initial settings

---
+++ src/config.lua
@@ -0,0 +1,8 @@
+local Config = {
+    Version = "1.0.0",
+    TimeoutPerTest = 3,
+    DebugConsole = true,
+    GithubRawUrl = "https://raw.githubusercontent.com/vxUNC/xUNC/main/build/init.lua"
+}
+
+return Config
